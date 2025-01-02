47b46ea53ec246f03eb0a55e8bacd0492ca8ab2c
tomas andrade <tandrade@icc.ub.edu>
2025-01-02 23:07:23 +0100

    try threading again

--- training_pipeline_edna_gpu.py
+++ training_pipeline_edna_gpu.py
@@ -232,14 +232,14 @@ def extract_f0_feature(gpus, n_p, f0method, if_f0, exp_dir, version, gpus_rmvpe)
                 cmd, shell=True, cwd=now_dir
             )  # , stdin=PIPE, stdout=PIPE,stderr=PIPE
             # 煞笔gr, popen read都非得全跑完了再一次性读取, 不用gr就正常读一句输出一句;只能额外弄出一个文本流定时读
-            # done = [False]
-            # threading.Thread(
-            #     target=if_done,
-            #     args=(
-            #         done,
-            #         p,
-            #     ),
-            # ).start()
+            done = [False]
+            threading.Thread(
+                target=if_done,
+                args=(
+                    done,
+                    p,
+                ),
+            ).start()
 
     leng = len(gpus)
     ps = []
