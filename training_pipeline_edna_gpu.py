7bed5249f474a5b40098a6dc299fa4ccd8bfda81
tomas andrade <tandrade@icc.ub.edu>
2025-01-02 22:49:16 +0100

    update script

--- training_pipeline_edna_gpu.py
+++ training_pipeline_edna_gpu.py
@@ -188,24 +188,24 @@ def preprocess_dataset(trainset_dir, exp_dir, sr, n_p):
     # threading stuff 
     # 煞笔gr, popen read都非得全跑完了再一次性读取, 不用gr就正常读一句输出一句;只能额外弄出一个文本流定时读
     
-    # done = [False]
-    # threading.Thread(
-    #     target=if_done,
-    #     args=(
-    #         done,
-    #         p,
-    #     ),
-    # ).start()
-    # while 1:
-    #     with open("%s/logs/%s/preprocess.log" % (now_dir, exp_dir), "r") as f:
-    #         yield (f.read())
-    #     sleep(1)
-    #     if done[0]:
-    #         break
-    # with open("%s/logs/%s/preprocess.log" % (now_dir, exp_dir), "r") as f:
-    #     log = f.read()
-    # logger.info(log)
-    # yield log
+    done = [False]
+    threading.Thread(
+        target=if_done,
+        args=(
+            done,
+            p,
+        ),
+    ).start()
+    while 1:
+        with open("%s/logs/%s/preprocess.log" % (now_dir, exp_dir), "r") as f:
+            yield (f.read())
+        sleep(1)
+        if done[0]:
+            break
+    with open("%s/logs/%s/preprocess.log" % (now_dir, exp_dir), "r") as f:
+        log = f.read()
+    logger.info(log)
+    yield log
 
 def extract_f0_feature(gpus, n_p, f0method, if_f0, exp_dir, version, gpus_rmvpe):
     gpus = gpus.split("-")
@@ -527,8 +527,6 @@ def train_index(exp_dir1, version19):
     #yield "\n".join(infos)
 
 preprocess_dataset(trainset_dir, exp_dir, sr, num_proc)
-
-next()
 
 # Need to give enough time for some folders to be created. This seems
 # very buggy! maybe create the folders from the beginning, instead of
