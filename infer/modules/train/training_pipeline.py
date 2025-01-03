55c8e46ef42362ee92f3fb54bc8e7e16543e88fd
tomas andrade <tandrade@icc.ub.edu>
2025-01-03 08:08:16 +0100

    add popen communicate

--- infer/modules/train/training_pipeline.py
+++ infer/modules/train/training_pipeline.py
@@ -79,28 +79,29 @@ def preprocess_dataset(trainset_dir, exp_dir, sr, n_p,
     
     logger.info("Execute: " + cmd)
     p = Popen(cmd, shell=True)
+    p.communicate()
 
     # threading stuff 
     # 煞笔gr, popen read都非得全跑完了再一次性读取, 不用gr就正常读一句输出一句;只能额外弄出一个文本流定时读
     
-    done = [False]
-    threading.Thread(
-        target=if_done,
-        args=(
-            done,
-            p,
-        ),
-    ).start()
-    while 1:
-        with open("%s/logs/%s/preprocess.log" % (now_dir, exp_dir), "r") as f:
-            yield (f.read())
-        sleep(1)
-        if done[0]:
-            break
-    with open("%s/logs/%s/preprocess.log" % (now_dir, exp_dir), "r") as f:
-        log = f.read()
-    logger.info(log)
-    yield log
+    # done = [False]
+    # threading.Thread(
+    #     target=if_done,
+    #     args=(
+    #         done,
+    #         p,
+    #     ),
+    # ).start()
+    # while 1:
+    #     with open("%s/logs/%s/preprocess.log" % (now_dir, exp_dir), "r") as f:
+    #         yield (f.read())
+    #     sleep(1)
+    #     if done[0]:
+    #         break
+    # with open("%s/logs/%s/preprocess.log" % (now_dir, exp_dir), "r") as f:
+    #     log = f.read()
+    # logger.info(log)
+    # yield log
 
 def extract_f0_feature(gpus, n_p, f0method, if_f0, exp_dir, version, gpus_rmvpe,
                        config_vars = None, 
@@ -128,7 +129,9 @@ def extract_f0_feature(gpus, n_p, f0method, if_f0, exp_dir, version, gpus_rmvpe,
             logger.info("Execute: " + cmd)
             p = Popen(
                 cmd, shell=True, cwd=now_dir
-            )  # , stdin=PIPE, stdout=PIPE,stderr=PIPE
+            ) 
+            p.communicate()
+            # , stdin=PIPE, stdout=PIPE,stderr=PIPE
             # 煞笔gr, popen read都非得全跑完了再一次性读取, 不用gr就正常读一句输出一句;只能额外弄出一个文本流定时读
             # done = [False]
             # threading.Thread(
@@ -159,7 +162,9 @@ def extract_f0_feature(gpus, n_p, f0method, if_f0, exp_dir, version, gpus_rmvpe,
         logger.info("Execute: " + cmd)
         p = Popen(
             cmd, shell=True, cwd=now_dir
-        )  # , shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=now_dir
+        )
+        p.communicate()
+        # , shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=now_dir
         ps.append(p)
 
 def click_train(
